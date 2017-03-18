56bb91b8e1283eda69a127bba408bcb4fccaefd9
yangx3 <yxy100good@gmail.com>
2017-03-18 15:02:11 -0700

    AI done
    All AI bugs fixed

--- src/main/java/edu/oregonstate/cs361/battleship/HardModel.java
+++ src/main/java/edu/oregonstate/cs361/battleship/HardModel.java
@@ -2,6 +2,7 @@ package edu.oregonstate.cs361.battleship;
 
 import java.util.ArrayList;
 import java.util.Random;
+import java.lang.*;
 
 /**
  * Created by geoscow on 3/18/17.
@@ -64,14 +65,11 @@ public class HardModel extends BattleshipModel {
 
     public boolean AIvalidShotTest(Coordinate shot) {
         //loops to check shot validity
-        for(Coordinate c: playerHits) {
-            if(shot.sameCoordinateTest(c))
-                return false;
-        }
-        for(Coordinate c: playerMisses) {
-            if(shot.sameCoordinateTest(c))
-                return false;
-        }
+        if(listcontains(playerHits, shot))
+            return false;
+        if(listcontains(playerMisses, shot))
+            return false;
+
         if(shot.getRow() > this.MAX || shot.getRow() < this.MIN
                 || shot.getCol() > this.MAX || shot.getCol() < this.MIN)
             return false;
@@ -84,21 +82,22 @@ public class HardModel extends BattleshipModel {
         Coordinate next_shot = null;
             for(Coordinate p : playerHits){
         System.out.println("Hit Coordinate: ("+p.getRow()+","+p.getCol()+")");}
-        if(Last_hit!=null){
-            System.out.println("Last Hit point: ("+Last_hit.getRow()+","+Last_hit.getCol()+")");
-            System.out.println(this.playerHits.contains(this.Last_hit));
-            System.out.println(this.engage);
-        }
+        //if(Last_hit!=null){
+          //  System.out.println("Last Hit point: ("+Last_hit.getRow()+","+Last_hit.getCol()+")");
+            //System.out.println(listcontains(playerHits, Last_hit));
+            //System.out.println(engage);
+        //}
 
-        if(this.playerHits.contains(this.Last_hit) && !this.engage){
-            this.engage=true;
-            this.mod=1;
-            this.mod_one_point=this.Last_hit;
+        if(listcontains(playerHits, Last_hit) && !engage){
+            engage=true;
+            mod=1;
+            mod_one_point=this.Last_hit;
         }
         if(!this.engage){
             return Random_firing();
         }
         if(mod==1){
+            //System.out.println("stage at Mod 1");
             next_shot=Found_line();
             if(next_shot==null){
                 engage=false;
@@ -108,10 +107,18 @@ public class HardModel extends BattleshipModel {
         }
         else if(mod==2){
             next_shot=in_line();
+            if(next_shot==null){
+                System.out.println("argc finish, wow");
+                engage=false;
+                mod=0;
+                next_shot=Random_firing();}
+
         }
         else if(mod==3){
+            System.out.println("Stage in mod 3");
             next_shot=other_side();
             if(next_shot==null){
+                System.out.println("argc finish, wow");
                 engage=false;
                 mod=0;
                 next_shot=Random_firing();
@@ -120,6 +127,7 @@ public class HardModel extends BattleshipModel {
         }
 
         //Last_hit=next_shot;
+        System.out.println("Shot point : (" + next_shot.getRow() + "," + next_shot.getCol()+")");
         return next_shot;
     }
 
@@ -137,37 +145,44 @@ public class HardModel extends BattleshipModel {
     //AI function Mode 1, find ship in cross
     private Coordinate Found_line(){
         if(modone_ini==0) {
+            //System.out.println("1");
             poss=new ArrayList<Coordinate>();
             Coordinate poss1 = new Coordinate(mod_one_point.getRow(), mod_one_point.getCol() - 1);
             Coordinate poss2 = new Coordinate(mod_one_point.getRow(), mod_one_point.getCol() + 1);
             Coordinate poss3 = new Coordinate(mod_one_point.getRow() - 1, mod_one_point.getCol());
             Coordinate poss4 = new Coordinate(mod_one_point.getRow() + 1, mod_one_point.getCol());
-            if (!AIvalidShotTest(poss1))
-                poss.add(poss1);
-            if (!AIvalidShotTest(poss2))
-                poss.add(poss2);
-            if (!AIvalidShotTest(poss3))
-                poss.add(poss3);
-            if (!AIvalidShotTest(poss4))
-                poss.add(poss4);
+            if (AIvalidShotTest(poss1)){
+                poss.add(poss1);}
+            if (AIvalidShotTest(poss2)){
+                poss.add(poss2);}
+            if (AIvalidShotTest(poss3)){
+                poss.add(poss3);}
+            if (AIvalidShotTest(poss4)){
+                poss.add(poss4);}
             modone_ini=1;
         }
         if(Last_hit==mod_one_point){
             Count_size++;
             return poss.get(Count_size-1);
         }
-        else if(!this.playerHits.contains(Last_hit) && Count_size <=4){
+        else if(!this.listcontains(playerHits, Last_hit) && Count_size <=poss.size()){
             Count_size++;
             return poss.get(Count_size-1);
         }
-        else if(this.playerHits.contains(Last_hit)){
+        else if(this.listcontains(playerHits, Last_hit)){
             Count_size=0;
             mod=2;
             modone_ini=0;
             countRow=mod_one_point.getRow()-Last_hit.getRow();
             countCol=mod_one_point.getCol()-Last_hit.getCol();
-            if(Last_hit.getRow()==mod_one_point.getRow())
-                return new Coordinate(Last_hit.getRow()+countRow, Last_hit.getCol()+countCol);
+            if(Last_hit.getRow()==mod_one_point.getRow() || Last_hit.getCol()==mod_one_point.getCol()){
+                Coordinate next=new Coordinate(Last_hit.getRow()-countRow, Last_hit.getCol()-countCol);
+                if(AIvalidShotTest(next))
+                    return next;
+                else {
+                    mod=3;
+                    return other_side();
+                }}
         }
         if(Count_size>=4){modone_ini=0;}
         return null;
@@ -175,33 +190,57 @@ public class HardModel extends BattleshipModel {
 
     //AI function Mode 2, use line found in mode one til it miss
     private Coordinate in_line(){
-        if(!this.playerHits.contains(Last_hit)){
+        System.out.println("Whenever call stage 2");
+        if(!listcontains(playerHits, Last_hit)){
+            System.out.println("Check this out!");
             mod=3;
             return other_side();
         }
-        Coordinate next=new Coordinate(Last_hit.getRow()+countRow, Last_hit.getCol()+countCol);
-        if(!AIvalidShotTest(next))
+        Coordinate next=new Coordinate(Last_hit.getRow()-countRow, Last_hit.getCol()-countCol);
+        System.out.println("In the center of stage 2");
+        if(AIvalidShotTest(next))
+            return next;
+        else
             return other_side();
-        return next;
 
     }
 
     //AI function mode 3, to different side til it miss
     private Coordinate other_side(){
         if(mod_three_ini==0) {
-            Coordinate next = new Coordinate(mod_one_point.getRow() - countRow, mod_one_point.getCol() - countCol);
+            //System.out.println("Mod 3 ini start");
+            Coordinate next = new Coordinate(mod_one_point.getRow() + countRow, mod_one_point.getCol() + countCol);
             mod_three_ini=1;
+            //System.out.println("ini done");
             if(AIvalidShotTest(next)){
+                //System.out.println("first shot after ini");
                 return next;
             }
-            else
-                return null;
+            else{
+                //System.out.println("Finish argc");
+                return null;}
         }
-        Coordinate next = new Coordinate(Last_hit.getRow()-countRow, Last_hit.getCol()-countCol);
-        if(AIvalidShotTest(next))
-            return next;
-        else
+        //System.out.println("still in argc");
+        Coordinate next = new Coordinate(Last_hit.getRow()+countRow, Last_hit.getCol()+countCol);
+        if(!listcontains(playerHits, Last_hit))
             return null;
+        if(AIvalidShotTest(next)){
+            //System.out.println("still in argc XD");
+            return next;}
+        else{
+            //System.out.println("finish ~");
+            return null;}
+    }
+
+    public boolean listcontains(ArrayList<Coordinate> list, Coordinate q){
+        for(Coordinate p : list){
+            if(p.getCol()==q.getCol()){
+                if(p.getRow()==q.getRow()){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
